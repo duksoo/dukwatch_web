ce732ecd81221944d8a01e7e8eb19d8b3d3532cf
조상범 <jsb_9905@naver.com>
2016-09-21 18:49:30 +0900

    모델 추가함

--- dukwatch/models.py
+++ dukwatch/models.py
@@ -7,3 +7,14 @@ class Post_sample(models.Model) :
     reg_date = models.DateTimeField(auto_created=True, auto_now=True)
     def __str__(self):
         return self.title
+
+class VideoBoard(models.Model) :
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    Day = models.DateTimeField(auto_created=True, auto_now=True)
+    URL = models.CharField(max_length=100)
+    UserName = models.CharField(max_length=20)
+    popular = models.CharField(max_length=10)
+    New = models.CharField(max_length=10)
+class Free_Board(models.Model) :
+    title = models.CharField()
