a4945d575b810c023080c5002dbfb93884bd4c1a
조상범 <lol2641@naver.com>
2016-11-02 18:43:42 +0900

    r

--- dukwatch/models.py
+++ dukwatch/models.py
@@ -8,7 +8,7 @@ class Post_sample(models.Model) :
     def __str__(self):
         return self.title
 
-class VideoBoard(models.Model) :
+class Video_Board(models.Model) :
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=100)
     Day = models.DateTimeField(auto_created=True, auto_now=True)
@@ -17,5 +17,36 @@ class VideoBoard(models.Model) :
     popular = models.CharField(max_length=10)
     New = models.CharField(max_length=10)
 
+class Ranking_Board(models.Model) :
+    Ranking = models.IntegerField(max_length=1000)
+    Title = models.CharField(max_length=100)
+    UserName = models.CharField(max_length=20)
+    Point = models.CharField(max_length=5)
+    Char =  models.CharField(max_length=10)
+    PlayTime = models.CharField(max_length=5)
+    KillDet = models.CharField(max_length=10)
+    mostchar = models.CharField(max_length=10)
+    KillDeath = models.CharField(max_length=10)
+    Tier = models.CharField(max_length=10)
+
+class Character(models.Model) :
+    userNo = models.IntegerField()
+    type = models.IntegerField() #1->경쟁전, 2-> 빠른대전
+    characterName = models.CharField(max_length=100) # 캐릭터 명이 들어감
+    kd = models.FloatField()
+
+
+class User_data(models.Model) :
+    Name = models.CharField(max_length=10)
+    Tier = models.CharField(max_length=10)
+    KillDeath = models.CharField(max_length=10)
+    Character = models.CharField(max_length=10)
+
+
+
 class Free_Board(models.Model) :
     title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    Day = models.DateTimeField(auto_created=True, auto_now=True)
+    UserName = models.CharField(max_length=20)
+
--- dukwatch/urls.py
+++ dukwatch/urls.py
@@ -1,17 +1,35 @@
 from django.conf.urls import url, include
 from rest_framework import routers, serializers, viewsets
-from dukwatch.models import VideoBoard
+from dukwatch.models import Video_Board, Free_Board, Ranking_Board
 
 class VideoSerializers(serializers.ModelSerializer) :
     class Meta:
-        model = VideoBoard
+        model = Video_Board
 
 class VideoBoardViewSet(viewsets.ModelViewSet) :
-    queryset = VideoBoard.objects.all()
+    queryset = Video_Board.objects.all()
+    serializer_class = VideoSerializers
+
+class FreeSerializers(serializers.ModelSerializer) :
+    class Meta:
+        model = Free_Board
+
+class FreeBoardViewSet(viewsets.ModelViewSet) :
+    queryset = Free_Board.objects.all()
+    serializer_class = VideoSerializers
+
+class RankingSerializers(serializers.ModelSerializer) :
+    class Meta:
+        model = Ranking_Board
+
+class RankingBoardViewSet(viewsets.ModelViewSet) :
+    queryset = Ranking_Board.objects.all()
     serializer_class = VideoSerializers
 
 router = routers.DefaultRouter()
 router.register(r'video', VideoBoardViewSet)
+router.register(r'free', FreeBoardViewSet)
+router.register(r'rank', RankingBoardViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
