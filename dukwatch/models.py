5f4dd1527c6b9c2cceb8e411497b1a58a28ce291
Administrator <lol2641@naver.com>
2016-11-18 15:54:04 +0900

    a

--- dukwatch/models.py
+++ dukwatch/models.py
@@ -17,30 +17,42 @@ class Video_Board(models.Model) :
     popular = models.CharField(max_length=10)
     New = models.CharField(max_length=10)
 
-class Ranking_Board(models.Model) :
-    Ranking = models.IntegerField(max_length=1000)
-    Title = models.CharField(max_length=100)
+class User_data(models.Model) :
+    characterName = models.CharField(max_length=100)  # 캐릭터 명이 들어감
     UserName = models.CharField(max_length=20)
     Point = models.CharField(max_length=5)
-    Char =  models.CharField(max_length=10)
     PlayTime = models.CharField(max_length=5)
-    KillDet = models.CharField(max_length=10)
-    mostchar = models.CharField(max_length=10)
-    KillDeath = models.CharField(max_length=10)
-    Tier = models.CharField(max_length=10)
+    Level = models.IntegerField(max_length=5)
 
-class Character(models.Model) :
-    userNo = models.IntegerField()
+class Character_data(models.Model) :
+    User = models.ForeignKey(User_data)
     type = models.IntegerField() #1->경쟁전, 2-> 빠른대전
-    characterName = models.CharField(max_length=100) # 캐릭터 명이 들어감
-    kd = models.FloatField()
-
-
-class User_data(models.Model) :
-    Name = models.CharField(max_length=10)
+    mostchar = models.CharField(max_length=10)
     Tier = models.CharField(max_length=10)
-    KillDeath = models.CharField(max_length=10)
-    Character = models.CharField(max_length=10)
+    WiningPer = models.CharField(max_length=10)
+    kd = models.FloatField(max_length=10)
+    runaway = models.timezone(max_length=10)
+    sumrunaway = models.timezone(max_length=15)
+    accuracy=models.CharField(max_length=10)
+    medal=models.CharField(max_length=5)
+    Gmedal = models.CharField(max_length=5)
+    Smedal = models.CharField(max_length=5)
+    Vmedal = models.CharField(max_length=5)
+    Mission = models.timezone(max_length=10)
+    BestMission = models.timezeon(max_length=10)
+    MissionKill = models.IntegerField(max_length=10)
+    BestMissionKill = models.IntegerField(max_length=5)
+    score = models.IntegerField(max_lenth=5)
+    Win = models.IntegerField(max_length=5)
+    Lose = models.IntegerField(max_length=5)
+    PiHaRang = models.IntegerField(max_length=10)
+    Death = models.IntegerField(max_length=10)
+    Deathave = models.FloatField(max_length=10)
+    Critical = models.IntegerField(max_length=10)
+    heal = models.IntegerField(max_length=20)
+    Kill = models.IntegerField(max_length=15)
+    SumKill = models.IntegerField(max_length=15)
+
 
 
 
@@ -49,4 +61,3 @@ class Free_Board(models.Model) :
     content = models.CharField(max_length=1000)
     Day = models.DateTimeField(auto_created=True, auto_now=True)
     UserName = models.CharField(max_length=20)
-
--- dukwatch/urls.py
+++ dukwatch/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers, serializers, viewsets
-from dukwatch.models import Video_Board, Free_Board, Ranking_Board
+from dukwatch.models import Video_Board, Free_Board, Character
 
 class VideoSerializers(serializers.ModelSerializer) :
     class Meta:
@@ -16,20 +16,29 @@ class FreeSerializers(serializers.ModelSerializer) :
 
 class FreeBoardViewSet(viewsets.ModelViewSet) :
     queryset = Free_Board.objects.all()
-    serializer_class = VideoSerializers
+    serializer_class = FreeSerializers
 
 class RankingSerializers(serializers.ModelSerializer) :
     class Meta:
-        model = Ranking_Board
+        model = Character
 
-class RankingBoardViewSet(viewsets.ModelViewSet) :
-    queryset = Ranking_Board.objects.all()
-    serializer_class = VideoSerializers
+class RankingViewSet(viewsets.ModelViewSet) :
+    queryset = Character.objects.all()
+    serializer_class = RankingSerializers
+
+class SearchDataSerializers(serializers.ModelSerializer) :
+    class Meta:
+        model = Character
+
+class SearchDataViewSet(viewsets.ModelViewSet) :
+    queryset = Character.objects.filter(UserNo=10)
+    serializer_class = SearchDataSerializers
 
 router = routers.DefaultRouter()
 router.register(r'video', VideoBoardViewSet)
 router.register(r'free', FreeBoardViewSet)
-router.register(r'rank', RankingBoardViewSet)
+router.register(r'rank', RankingViewSet)
+router.register(r'search', SearchDataViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
